31d77db5bd3a72cd66e210aa050d344fa9f6d84a
diego.costa <diego.costa@rpinfo.com.br>
2025-03-24 16:22:48 -0300

    ✨ :: [Cadastro] :: Implementa página de Cadastro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import {RegisterComponent} from "./components/register/register.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: RegisterComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  }
+];
